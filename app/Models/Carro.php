b56f3f0abe423d7bd4fd4a4bf946797bffa67a29
Angelo <62099585+acmachado14@users.noreply.github.com>
2021-11-30 21:08:47 -0300

    update Model Carro

--- app/Models/Carro.php
+++ app/Models/Carro.php
@@ -42,6 +42,19 @@ class Carro extends Model
         $this->ano = $ano;
         $this->descricao = $descricao;
     }
+
+    public static function fromArray(array $data): self
+    {
+        $car = new self();
+        $car->placa = $data['placa'];
+        $car->chassi = $data['chassi'];
+        $car->combustivel = $data['combustivel'];
+        $car->lugar = $data['lugar'];
+        $car->ano = $data['ano'];
+        $car->descricao = $data['descricao'];
+        
+        return $car;
+    }
     
 }
 
